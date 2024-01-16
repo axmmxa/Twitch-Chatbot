7ffc9be0ca0d2712cc168d2d3f589405eba2c40a
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2024-01-16 16:35:23 -0500

    client outsorced

--- NodeJsChatbot/src/app.js
+++ NodeJsChatbot/src/app.js
@@ -1,1 +1,6 @@
+
+import {MAIN_CLIENT} from "./clients.js"
+
 console.log("app loaded")
+
+MAIN_CLIENT.connect()
---
+++ NodeJsChatbot/src/clients.js
@@ -0,0 +1,19 @@
+import tmi from "tmi.js"
+
+const options0 = {
+    options: {
+        debug: true },
+    connection: {
+        cluster: "aws",
+        reconnect: "true",
+    },
+    identity: {
+		username: 'alextarifa0812',
+		password: 'oauth:iqucb1681s3gn6s4g2ze253ladzzol'
+	},
+    channels: [ 'AnneMunition' ]
+}
+
+const client0 = new tmi.client(options0)
+
+export const MAIN_CLIENT = client0
