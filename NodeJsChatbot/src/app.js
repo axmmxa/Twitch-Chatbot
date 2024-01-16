60548ccf34e88643635c625cf54d6e449925c157
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2024-01-16 17:03:38 -0500

    update

--- NodeJsChatbot/src/app.js
+++ NodeJsChatbot/src/app.js
@@ -3,4 +3,11 @@ import {MAIN_CLIENT} from "./clients.js"
 
 console.log("app loaded")
 
+
+
+//Main Client
 MAIN_CLIENT.connect()
+
+MAIN_CLIENT.on("message", (channel, message, userstate, self) =>{
+    console.log( "Die Nachricht war:" + message)
+})
